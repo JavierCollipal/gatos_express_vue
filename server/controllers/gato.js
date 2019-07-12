e8aa2b02ca80addedbd68d2565157665de655456
Javier Collipal <jcollipal1212@gmail.com>
2019-07-12 17:23:28 -0400

    Update mediante nombre funcionando

--- server/controllers/gato.js
+++ server/controllers/gato.js
@@ -31,12 +31,11 @@ exports.traerTodos = async (request, response, next) => {
 
     await gato.find()
         .then(documento => {
-            response.json(documento);
+            response.status(200).json(documento);
         })
         .catch(error => {
             console.error(error);
-            response.json({
-                status: 400,
+            response.status(400).json({
                 mensaje: 'no existen gatos creados'
             });
         });
@@ -48,28 +47,36 @@ exports.crearGato = async (request, response, next) => {
 
     await documento.save()
         .then(documento => {
-            response.json({
-                status: 200,
+            response.status(200).json({
                 mensaje: 'gato creado con exito'
             });
         })
         .catch(error => {
             console.error(error);
-            response.json({
-                status: 400,
+            response.status(400).json({
                 mensaje: 'no fue posible crear el gato'
             });
         });
 }
 
-exports.actualizarGatoPorNombre = async(request, response, next) => {
-    let nombreGato  = request.query.nombre;
-    await gato.findOne({nombre: nombreGato})
-    .then(esquema =>{
-    response.status(200).jsonp( esquema );
-    })
-    .catch(error =>{
-        response.status(400).jsonp( error );
-    });
+exports.actualizarGatoPorNombre = async (request, response, next) => {
+    //query para buscar por nombre
+    let queryBusqueda = { nombre: request.query.nombreParaBusqueda };
+    
+
+    let queryActualizar = {
+        nombre: request.query.nombre,
+        apellido: request.query.apellido,
+        complexion: request.query.complexion,
+        edad: parseInt(request.query.edad),
+        animo: request.query.animo
+    };
+    await gato.findOneAndUpdate(queryBusqueda, queryActualizar)
+        .then(() => {
+            response.status(200).json({mensaje: 'Gato actualizado con exito'});
+        })
+        .catch(error => {
+            response.status(400).jsonp(error);
+        });
 }
 
